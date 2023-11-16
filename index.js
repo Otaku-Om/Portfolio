f08c2da90930d66350762af69465404479be5f52
Otaku-Om <saxenaom63975@gmail.com>
2023-11-16 22:15:38 +0530

    new project added in portfolio

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const tabLinksEl = document.querySelectorAll(".tab-links");
 const tabContentsEl = document.querySelectorAll(".tab-contents");
-console.log(tabLinksEl);
-console.log(tabContentsEl);
+const seeMoreEl = document.getElementById("seeMore");
+const workListEl = document.querySelectorAll(".work");
+// console.log(workListEl);
+// console.log(seeMoreEl);
+// console.log(tabLinksEl);
+// console.log(tabContentsEl);
 
 tabLinksEl.forEach((tablink) => {
    tablink.addEventListener("click", openTab);
@@ -33,3 +37,29 @@ function openMenu() {
 function closeMenu() {
    sideMenuEl.style.right = "-200px";
 }
+
+let filteredWorkList = [];
+
+for(let i = 0; i < workListEl.length; i++){
+   if(workListEl[i].classList.contains("seeMore")){
+      filteredWorkList = [...filteredWorkList, workListEl[i]];
+   }
+}
+
+seeMoreEl.addEventListener("click", (e) => {
+   if(e.target.textContent === "See More"){
+      filteredWorkList.forEach((work) => {
+         work.classList.remove("seeMore");
+      });
+      e.target.textContent = "See Less" ;
+   }
+   else{
+      filteredWorkList.forEach((work) => {
+         work.classList.add("seeMore");
+      });
+      e.target.textContent = "See More" ;
+   }
+})
+
+
+console.log(filteredWorkList);
